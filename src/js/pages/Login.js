994cd391c3a81bd1a6299fff6f9840bbbbf35f56
Pop Alexandra Maria <alexandra.p21m@gmail.com>
2018-03-21 17:53:56 +0200

    Added more stuff to login page.

--- src/js/pages/Login.js
+++ src/js/pages/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import FacebookLogin from "react-facebook-login";
 import GoogleLogin from "react-google-login";
-
+import "../../css/login.css";
 import { apiPost } from "../utils/Api";
 
 export default class Login extends React.Component {
@@ -12,15 +12,14 @@ export default class Login extends React.Component {
             error: false,
         };
 
-        this.facebookLoginResponse = this.facebookLoginResponse.bind( this );
-        this.googleLoginResponse = this.googleLoginResponse.bind( this );
-        this.callFacebookLoginApi = this.callFacebookLoginApi.bind( this );
+        this.socialLoginResponse = this.socialLoginResponse.bind( this );
+        this.callSocialLoginApi = this.callSocialLoginApi.bind( this );
         this.accessToken = "";
     }
 
-    callFacebookLoginApi() {
+    callSocialLoginApi( providerType ) {
         apiPost(
-            "/api/users/auth/facebook",
+            `/api/users/auth/${ providerType }`,
             { access_token: this.accessToken },
         )
             .then( payload => {
@@ -34,38 +33,65 @@ export default class Login extends React.Component {
             } );
     }
 
-    facebookLoginResponse( response ) {
+    socialLoginResponse( response, providerType ) {
         const { accessToken } = response;
         this.accessToken = accessToken;
-    }
-
-    googleLoginResponse( response ) {
-        console.log( this );
-        console.log( response );
+        if ( !accessToken || accessToken === "" ) {
+            this.setState( { error: true } );
+            return;
+        }
+        if ( providerType === "google" ) {
+            this.callSocialLoginApi( providerType );
+        }
     }
 
     render() {
         const { error } = this.state;
 
         return (
-            <div>
-                { error && <h3>Something went wrong when trying to login.</h3>}
-                <FacebookLogin
-                    appId="899445726913963"
-                    autoLoad
-                    fields="name,email,picture"
-                    cssClass="fb-login-button"
-                    textButton=""
-                    callback={ this.facebookLoginResponse }
-                    onClick={ this.callFacebookLoginApi }
-                    onFailure={ this.facebookLoginResponse }
-                />
-                <GoogleLogin
-                    clientId="194832330236-ukd3dkogrp3itq90tc3mcnb0h04ku3tb.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={ this.googleLoginResponse }
-                    onFailure={ this.googleLoginResponse }
-                />
+            <div className="login-page">
+                <div className="login-container">
+                    <h1 className="login-title">Welcome back</h1>
+
+                    <div className="main-container">
+                        <div className="login-form-container">
+                            <h4 className="login-message">
+                    Please enter your details to login into your account.
+                            </h4>
+                        </div>
+
+                        { error && <h3>Something went wrong while trying to login.</h3>}
+                        <div className="social-icons-container">
+                            <h3>or login using one of your social accounts</h3>
+                            <FacebookLogin
+                                appId="899445726913963"
+                                autoLoad
+                                fields="name,email,picture"
+                                cssClass="social-login-button"
+                                textButton=""
+                                callback={ ( response ) => {
+                                    this.socialLoginResponse( response, "facebook" );
+                                } }
+                                onClick={ this.callFacebookLoginApi }
+                                onFailure={ ( response ) => {
+                                    this.socialLoginResponse( response, "facebook" );
+                                } }
+                            />
+
+                            <GoogleLogin
+                                clientId="194832330236-ukd3dkogrp3itq90tc3mcnb0h04ku3tb.apps.googleusercontent.com"
+                                buttonText=""
+                                className="social-login-button"
+                                onSuccess={ ( response ) => {
+                                    this.socialLoginResponse( response, "google" );
+                                } }
+                                onFailure={ ( response ) => {
+                                    this.socialLoginResponse( response, "google" );
+                                } }
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
